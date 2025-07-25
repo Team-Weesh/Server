f670f04747785c8e36d67e7574052fc55db0adec
SeungHwan <silofn523@gmail.com>
2025-07-25 16:25:42 +0900

    Add files via upload

---
+++ src/main/java/com/example/weesh/WeeshApplication.java
@@ -0,0 +1,12 @@
+package com.example.weesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeshApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeeshApplication.class, args);
+    }
+
+}
