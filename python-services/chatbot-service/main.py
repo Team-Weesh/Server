e1edcd97550b865a85cfce4386ed90e5022fe1b2
seungbo <seungboi684@gmail.com>
2025-08-14 17:14:24 +0900

    chatbot main.py 파일을 통한 서버 실행

--- python-services/chatbot-service/main.py
+++ python-services/chatbot-service/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from app.api.routes import chatbot
+import uvicorn
+
 app = FastAPI()
 app.include_router(chatbot.router)
 
 @app.get("/")
 async def root():
-  return {"Message":"Go to loacalhost:8000/docs",
-          "AAAA" : "/docs 로 가세여"}
+  return {"Message":"Go to localhost:8000/docs",
+          "AAAA" : "/docs 로 가세요"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
