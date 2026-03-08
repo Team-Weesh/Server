b9c3fa31f0df538d8d5d753976316c5fb5632d7d
KwonJH614 <madogkwon@gmail.com>
2026-03-09 01:12:02 +0900

    feat: 상담 예약을 로그인한 사용자만 가능하도록 변경

--- src/main/java/com/example/weesh/core/advice/application/factory/AdviceFactory.java
+++ src/main/java/com/example/weesh/core/advice/application/factory/AdviceFactory.java
@@ -4,5 +4,5 @@ import com.example.weesh.core.advice.domain.Advice;
 import com.example.weesh.web.advice.dto.AdviceCreateRequestDto;
 
 public interface AdviceFactory {
-    Advice createAdvice(AdviceCreateRequestDto dto, Long userId);
+    Advice createAdvice(AdviceCreateRequestDto dto, Long userId, Integer studentName, String fullName);
 }
